53571ea691e2b19e9f015fc285a394628c3bba6f
Patrick Hamann <patrick.hamann@guardian.co.uk>
2015-05-11 22:16:43 +0100

    Add default system env exports

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,14 @@
-export EDITOR='atom'
+export GITHUB_USER="phamann"
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH # Reorder PATH so local bin is first
+export GREP_OPTIONS='--color=auto'
+export GREP_COLOR='1;32'
+export MANPAGER="less -X" # Don’t clear the screen after quitting a manual page
+export EDITOR="vim"
+export TERM="screen-256color"
+export CLICOLOR=1
+export LSCOLORS=Gxfxcxdxbxegedabagacad
+export LS_COLORS=Gxfxcxdxbxegedabagacad
+export JAVA_HOME=$(/usr/libexec/java_home)
+export PATH="/usr/local/heroku/bin:$PATH"
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+
