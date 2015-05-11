010b5abeaad337d865412417e6a603801cb69eab
Patrick Hamann <patrick.hamann@guardian.co.uk>
2015-05-11 23:10:10 +0100

    Update homebrew install script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,7 +12,34 @@ then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
 # Install homebrew packages
 brew install grc coreutils spark
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+# Install Bash 4
+brew install bash
+
+# Install wget with IRI support
+brew install wget --enable-iri
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+brew tap josegonzalez/homebrew-php
+
+# Install everything else
+brew install ack
+brew install git
+brew install rename
+brew install tree
+
+brew cleanup
 
 exit 0
