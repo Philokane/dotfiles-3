03448b35eccfb13747101f308ef75dd2e7f3f6bc
Patrick Hamann <patrick.hamann@guardian.co.uk>
2015-05-11 21:50:41 +0100

    Add zsh bindings file

---
+++ zsh/bindings.zsh
@@ -0,0 +1,11 @@
+# Make the delete key (or Fn + Delete on the Mac) work instead of outputting a ~
+bindkey '^?' backward-delete-char
+bindkey "^[[3~" delete-char
+bindkey "^[3;5~" delete-char
+bindkey "\e[3~" delete-char
+
+# Make the `beginning/end` of line and `bck-i-search` commands work within tmux
+bindkey '^R' history-incremental-search-backward
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
+
