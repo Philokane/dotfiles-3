7f0d4ae8c7cd30ba47bde178f26abdc6cb4438ca
Patrick Hamann <patrick.hamann@guardian.co.uk>
2015-05-14 17:26:22 +0100

    Install nave to manage node virtual envs

---
+++ node/install.sh
@@ -0,0 +1,5 @@
+# Install the latest stable nave the node.js environment switcher Node.js
+curl -fsSL https://raw.github.com/isaacs/nave/master/nave.sh > /usr/local/bin/nave && chmod ugo+x /usr/local/bin/nave
+
+# Install a global node.js
+nave usemain stable
