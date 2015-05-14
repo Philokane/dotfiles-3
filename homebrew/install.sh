d891b44ae41c3058025620a3c1633ecce2ea4666
Patrick Hamann <patrick.hamann@guardian.co.uk>
2015-05-14 17:25:59 +0100

    Add osx apps via brew cask

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -40,6 +40,39 @@ brew install git
 brew install rename
 brew install tree
 
+#Install zsh
+brew install zsh
+sudo sh -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
+chsh -s /usr/local/bin/zsh
+
+brew install
+
+# Install homebrew-cask for easily installing application binaries
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+brew install caskroom/cask/brew-cask
+
+brew tap caskroom/versions
+
+brew cask install sublime-text-dev
+brew cask install google-chrome
+brew cask install google-chrome-canary
+brew cask install iterm2-nightly
+brew cask install alfred
+brew cask install divvy
+brew cask install vlc
+brew cask install charles
+brew cask install imageoptim
+brew cask install slack
+brew cask install sonos
+brew cask install spotify
+brew cask install bittorrent-sync
+
+# Install fonts
+brew tap caskroom/fonts
+
+brew cask install font-source-code-pro
+brew cask install font-sauce-code-powerline
+
 brew cleanup
 
 exit 0
