8c85e9daf1307cf6959cdf5e6bd1f0b230362608
phamann <patrickhamann@gmail.com>
2016-12-29 16:46:58 +0000

    Add node global deps

--- node/install.sh
+++ node/install.sh
@@ -1,10 +1,10 @@
-# Install the latest stable nave the node.js environment switcher Node.js
-curl -fsSL https://raw.github.com/isaacs/nave/master/nave.sh > /usr/local/bin/nave && chmod ugo+x /usr/local/bin/nave
-
-# Install a global node.js
-nave usemain stable
-
 # Install global deps
 npm install -g jshint
-npm install -g grunt
-npm install -g gulp
+npm install -g eslint
+npm install -g eslint_d
+npm install -g typescript
+npm install -g babel-cli
+npm install -g babel-eslint
+npm install -g bower
+npm install -g rollup
+npm install -g vtop
