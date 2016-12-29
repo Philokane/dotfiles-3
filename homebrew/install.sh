d618db4d95134d67e8133c05dd65468ed3989088
phamann <patrickhamann@gmail.com>
2016-12-29 17:06:33 +0000

    Refactor install scripts

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -9,81 +9,16 @@
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  fi
+
 fi
-
-# Make sure we’re using the latest Homebrew
-brew update
-
-# Upgrade any already-installed formulae
-brew upgrade
-
-# Install homebrew packages
-brew install grc coreutils spark
-
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
-# Install Bash 4
-brew install bash
-
-# Install wget with IRI support
-brew install wget --enable-iri
-
-# Install more recent versions of some OS X tools
-brew tap homebrew/dupes
-brew install homebrew/dupes/grep
-brew tap josegonzalez/homebrew-php
-
-# Install everything else
-brew install ack
-brew install git
-brew install hub
-brew install rename
-brew install tree
-brew install z
-brew install tmux
-brew install mongodb
-brew install nghttp2
-brew install httpie
-brew install redis
-brew install jq
-brew install openssl
-brew install node
-
-#Install zsh
-brew install zsh
-sudo sh -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
-chsh -s /usr/local/bin/zsh
-
-# Install homebrew-cask for easily installing application binaries
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-brew tap caskroom/cask
-brew tap caskroom/versions
-
-brew cask install google-chrome
-brew cask install google-chrome-canary
-brew cask install iterm2-nightly
-brew cask install alfred
-brew cask install divvy
-brew cask install dash
-brew cask install dropbox
-brew cask install vlc
-brew cask install charles
-brew cask install imageoptim
-brew cask install slack
-brew cask install sonos
-brew cask install spotify
-brew cask install licecap
-brew cask install java
-brew cask install postman
-brew cask install docker
-brew cask install ngrok
-
-# Install fonts
-brew tap caskroom/fonts
-brew cask install font-source-code-pro-for-powerline
-brew cask install font-source-code-pro
-
-brew cleanup
 
 exit 0
